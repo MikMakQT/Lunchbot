71ab51f28a9e84cebaeb20fc7669c2733d904f17
MikMakQT <122343662+MikMakQT@users.noreply.github.com>
2024-01-25 08:33:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import slack
 from slack_sdk import WebClient
 from bs4 import BeautifulSoup 
 
@@ -6,7 +5,6 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-from fpdf import FPDF
 import requests
 import time
 from datetime import date
@@ -24,27 +22,11 @@ client = WebClient(token=os.environ['SLACK_TOKEN'])
 
 
 def find_ruoka():
-    #RUOKALISTA SIVUN KOKO HTML TEKSTI
-    html_text = requests.get('https://wolkoff.fi/ruoka-juoma/').text
-    #SIVULLE TEHTIIN JOTAIN XD
-    soup = BeautifulSoup(html_text,'lxml')
-    #LÖYDETÄÄN KAIKKI LI, MENU-LIST_ITEM, koska sieltä löytyy ruokalistan sisältö
-    datas = soup.find_all('li', class_="menu-list__item")
-
-    #PÄIVÄMÄÄRÄ TÄNÄÄN
-    today = date.today()
-
-    datetime_month = today.month
-    datetime_day = today.day
-    #YHDISTETÄÄN PÄIVÄ JA KUUKAUSI
-    aika_tänään = (f'{datetime_day}.{datetime_month}')
-    #MUUTETAAN STRING MUOTOON
-    inputText = aika_tänään
-    
+       
     for data in datas:
         data = data.text
         #JOS TÄMÄ PÄIVÄMÄÄRÄ LÖYTYY RUOKALISTALTA, ruokalista tulostuu slackkiin
-        if inputText in data:
+        if aika() in data:
             print(data, end='')
             kaava = data
             client.chat_postMessage(channel='#lunch-bot', text = (f'Tänään ruokana:\n {kaava}'))
